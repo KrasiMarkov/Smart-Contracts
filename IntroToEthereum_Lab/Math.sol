31f61c4ef9a13118f88c966587e7f1cd119f0781
KrasiMarkov <krasimarkov5@gmail.com>
2023-02-08 16:38:19 +0200

    Create contract Math

--- IntroToEthereum_Lab/Math.sol
+++ IntroToEthereum_Lab/Math.sol
@@ -1,8 +1,21 @@
 pragma solidity 0.4.24;
 
-contract AddNumbers {
-    function AddNum(uint256 a, uint256 b) public returns (uint256){
+contract Math {
+
+    function Add(uint256 a, uint256 b) public returns (uint256){
         return a + b;
+    }
+
+    function Sub(uint256 a, uint256 b) public returns (uint256){
+       return a - b;
+    }
+
+    function Mul(uint256 a, uint256 b) public returns (uint256) {
+        return a * b;
+    }
+
+    function Div(uint256 a, uint256 b) public returns(uint256){
+      return a / b;
     }
 }
 
