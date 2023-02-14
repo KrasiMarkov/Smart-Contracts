3ecee7659db094189d72b93a23943e7141ddfd17
KrasiMarkov <krasimarkov5@gmail.com>
2023-02-14 11:52:42 +0200

    Created Owned contract

---
+++ Basics_of_contracts/Owned.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.4.24;
+
+contract Owned {
+
+    uint state;
+    address owner;
+
+    constructor() public {
+
+        owner = msg.sender;
+
+    }
+
+    modifier onlyOwner {
+
+        require(msg.sender == owner, "Can be invoked by the owner only!");
+        _;
+    }
+
+    function kill() public onlyOwner {
+
+        selfdestruct(owner);
+
+    }
+
+    function setNewState(uint _state) public onlyOwner {
+
+        state = _state;
+    }
+
+    function getOwnerBalance() public view returns(uint 256){
+
+        return owner.balance;
+    }
+}
