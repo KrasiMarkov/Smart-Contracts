d720c18fdf57bea2f145797d4946545a16553ef6
KrasiMarkov <krasimarkov5@gmail.com>
2023-02-16 16:48:12 +0200

    Created Ownership contract

--- Basics_of_contracts/Owned.sol
+++ Basics_of_contracts/Owned.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.24;
 
-contract Owned {
+contract Ownership {
 
     uint state;
     address owner;
---
+++ Basics_of_contracts/Ownership.sol
@@ -0,0 +1,56 @@
+pragma solidity 0.4.24;
+
+contract OwnerLab {
+
+    address public owner;
+
+    address public proposedOwner;
+
+    uint timeLimit;
+
+    event OwnershipTransferred (address indexed priviousOwner, address indexed newOwner); 
+    event OwnershipAccepted (address indexed newOwner); 
+    
+
+    constructor() public {
+
+       owner = msg.sender;
+    }
+
+
+    modifier onlyOwner {
+
+        require(msg.sender == owner, "Can be invoked by the owner only!");
+        _;
+    }
+
+    modifier onlyProposedOwner {
+
+        require(msg.sender == proposedOwner, "Can be invoked by the proposed owner only!");
+        _;
+    }
+    
+
+    function transferOwnership(address newOwner) public onlyOwner {
+
+        require(newOwner != address(0x0))
+
+        emit OwnershipTransferred (owner, newOwner); 
+
+        proposedOwner = newOwner;
+
+        timeLimit = now;
+
+    }
+
+    function acceptOwnership() public onlyProposedOwner {
+
+        require(now < timeLimit + 30 seconds);
+
+        emit OwnershipAccepted (msg.sender);
+
+        owner = msg.sender;
+    }
+
+
+}
