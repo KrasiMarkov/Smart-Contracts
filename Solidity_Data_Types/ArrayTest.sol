457f9bd45900cae8ce7c6ea67599aca6ebb507e2
KrasiMarkov <krasimarkov5@gmail.com>
2023-02-28 16:52:36 +0200

    Created ArrayTest contract

---
+++ Solidity_Data_Types/ArrayTest.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.4.24 ;
+
+
+contract ArrayTest{
+
+    //automatic index getter
+    uint[3] public threeInts = [1,2,3];
+
+    //automatic index getter
+    uint[] public manyInts;
+    
+    function increment(uint8 index) public {
+
+        threeInts[index] += 1;
+
+    }
+    
+    function push(uint newInt) public {
+
+        manyInts.push(newInt);
+
+
+        //same as:
+        manyInts.length += 1;
+        manyInts[manyInts.length-1] = newInt;
+    }
+    
+    //warning: only works if called from the outside, not a contract
+    function getManyInts() public view returns(uint[]){
+
+        return manyInts;
+
+    }
+    
+    function getThreeInts() public view returns (uint[3]){
+
+        return threeInts;
+        
+    }
+}
